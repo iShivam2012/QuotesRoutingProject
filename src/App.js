108f2ff31edb8b302cbc7059808d86d6cbe3ec4a
iShivam2012 <shivam.gupta97a@gmail.com>
2021-07-22 02:58:33 +0530

    implemented lazy loading by using react.lazy() and Suspense with fallback prop

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NoPageFound from "./pages/NoPageFound";
-import QuoteDetails from "./pages/QuoteDetails";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NoPageFound = React.lazy(() => import("./pages/NoPageFound"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quotesId">
-          <QuoteDetails />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NoPageFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quotesId">
+            <QuoteDetails />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NoPageFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
